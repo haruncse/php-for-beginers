d4b320789aa8cb5bfbdc6a910b21845efa495529
Harun Rashid <harunorangebd.com>
2025-04-18 10:59:32 +0600

    Addition | Conditional Statement | Added

---
+++ Arithmatic_Operations/arithmatic_operations.php
@@ -0,0 +1,19 @@
+<?php
+
+$variable1 = 40;
+$variable2 = 50;
+
+print("Value 1: ".$variable1." Value 2: ".$variable2);
+
+echo "<br> addition <br>";
+print $variable1 + $variable2;
+echo "<br>substraction <br>";
+print $variable1 - $variable2;
+echo "<br>multiplications <br>";
+print $variable1 * $variable2;
+echo "<br> Divission <br>";
+print $variable1 / $variable2;
+
+//this is comments
+
+?>
---
+++ Conditional_Statement/conditional_statement.php
@@ -0,0 +1,30 @@
+<?php
+
+$variable1 = 40;
+$variable2 = 50;
+
+print("Value 1: ".$variable1." Value 2: ".$variable2);
+
+echo "<br> addition <br>";
+print $variable1 + $variable2;
+echo "<br>substraction <br>";
+print $variable1 - $variable2;
+$variable3=$variable1 - $variable2;
+echo "<br>multiplications <br>";
+print $variable1 * $variable2;
+echo "<br> Divission <br>";
+print $variable1 / $variable2;
+
+//this is comments
+echo "<br>";
+
+if($variable3> 0){
+    print("Positive Value");
+}else{
+    print("Negative Value");
+}
+
+$variable=30;
+
+// Even Odd Detection 
+?>
