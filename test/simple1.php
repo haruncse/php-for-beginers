c3484d3ee19940082063b5182373f5300f34d72f
Harun Rashid <harunorangebd.com>
2025-04-22 09:57:53 +0600

    Basic Updated

---
+++ test/simple1.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "This is from PHP Echo <br>";
+print (" Hello World print ");
+print_r("<br> Hello  print_r!!");
+var_dump("<br> Hello Dump");
+
+$variable1 = 10;
+$variable2 = 40;
+$sumResult = $variable1 + $variable2;
+echo "<br> Result: ".$sumResult;
+
+
+
+?>
---
+++ test/simple_php_file.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <?php   echo "This is from PHP Echo <br>" ?>
+    
+    <h1>This is HTML Tag From HTML H1</h1>
+
+    <?php
+
+    echo "Hello Every One echo  <br>";
+    print (" Hello World print ");
+    // $a[]=[1,2,3,4];
+    print_r("<br> Hello  print_r!!", );
+    var_dump("<br> Hello Dump");
+
+    ?>
+
+</body>
+
+</html>
